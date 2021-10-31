db69a7787a59aa7c8f598be1bdd4c58b2f86758b
minusInfinite <ashley@minusinfinite.net>
2021-10-31 20:34:31 +1030

    server configured for graphql

--- server/server.js
+++ server/server.js
@@ -1,37 +1,53 @@
 require("dotenv").config()
+const {
+    ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core")
 const { ApolloServer } = require("apollo-server-express")
 const express = require("express")
+const morgan = require("morgan")
 const path = require("path")
 const db = require("./config/connection")
-const routes = require("./routes")
+//const routes = require("./routes")
+const { typeDefs, resolvers } = require("./schemas/")
+const { authMiddleware } = require("./utils/auth")
 
-async function startApolloServer(typeDef, resolvers) {
+async function startApolloServer(typeDefs, resolvers) {
     const app = express()
     const PORT = process.env.PORT || 3001
     const gqlServer = new ApolloServer({
         typeDefs,
         resolvers,
-        plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
+        context: ({ req, res }) => {
+            return authMiddleware({ req: req })
+        },
+        plugins: [
+            ApolloServerPluginLandingPageGraphQLPlayground({
+                settings: { "request.credentials": "include" },
+            }),
+        ],
     })
 
     await gqlServer.start()
-    gqlServer.applyMiddleware({ app })
 
     app.use(express.urlencoded({ extended: true }))
     app.use(express.json())
+
+    gqlServer.applyMiddleware({ app })
 
     // if we're in production, serve client/build as static assets
     if (process.env.NODE_ENV === "production") {
         app.use(express.static(path.join(__dirname, "../client/build")))
     }
 
-    app.use(routes)
+    //app.use(routes)
 
     db.once("open", () => {
         app.listen(PORT, () =>
             console.log(
-                `🌍 Now listening on localhost:${PORT}\nUse GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+                `🌍 Now listening on localhost:${PORT}\nUse GraphQL at http://localhost:${PORT}${gqlServer.graphqlPath}`
             )
         )
     })
 }
+
+startApolloServer(typeDefs, resolvers).catch((err) => console.error(err))
