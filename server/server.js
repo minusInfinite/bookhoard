9cd7df1fa5ceb8490c120a6af15880f58b930b5b
minusInfinite <ashley@minusinfinite.net>
2021-10-31 10:54:32 +1030

    adding apollo for graphql refactor

--- server/server.js
+++ server/server.js
@@ -1,21 +1,37 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+require("dotenv").config()
+const { ApolloServer } = require("apollo-server-express")
+const express = require("express")
+const path = require("path")
+const db = require("./config/connection")
+const routes = require("./routes")
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+async function startApolloServer(typeDef, resolvers) {
+    const app = express()
+    const PORT = process.env.PORT || 3001
+    const gqlServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
+    })
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+    await gqlServer.start()
+    gqlServer.applyMiddleware({ app })
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.json())
+
+    // if we're in production, serve client/build as static assets
+    if (process.env.NODE_ENV === "production") {
+        app.use(express.static(path.join(__dirname, "../client/build")))
+    }
+
+    app.use(routes)
+
+    db.once("open", () => {
+        app.listen(PORT, () =>
+            console.log(
+                `🌍 Now listening on localhost:${PORT}\nUse GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+            )
+        )
+    })
 }
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
