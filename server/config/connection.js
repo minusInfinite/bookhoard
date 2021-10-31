ab73634fb76f87c529e05354dfb96417d980113e
minusInfinite <ashley@minusinfinite.net>
2021-10-31 10:53:22 +1030

    format and hosting setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
+require("dotenv").config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connection
